b81eac4404b17598e7ffd722f4d7be729e68d727
SelvaS66 <selvamsmani56@gmail.com>
2026-07-09 10:13:14 +0530

    commit

---
+++ If_Medium.py
@@ -0,0 +1,54 @@
+
+# ###ATM WITHDRAWAL###
+# balance = 6000
+# withdraw = int(input("Enter withdrawal amount: "))
+# if withdraw > balance:
+#     print("Insufficient Balance")
+# elif withdraw <=0:
+#     print("Enter a valid amount")    
+# else:
+#     print("Please get your cash")
+#     balance -= withdraw
+#     print(f"your balance is {balance}")
+
+
+
+####Password Checker####
+# password = input("Enter your Password: ")
+# if len(password) <= 5:
+#     print("Password is Weak")
+# elif len(password) > 5 and len(password) <=10:
+#     print("Password is Medium")
+# elif len(password) > 10 and len(password) <=15:
+#     print("Password is Strong")
+# else:
+#     print("Enter password less than 15 characters")
+
+
+
+####Traffic light####
+
+# light = input("Enter the traffic light color: ")
+# if light.capitalize() == "Red":
+#     print("Stop.")
+# elif light.capitalize() == "Green":
+#     print("GO")
+# elif light.capitalize() == "Yellow":
+#     print("Wait")
+# else:
+#     print("Invalid Color")
+
+
+###Password Validator###
+
+# username = "Selvams6636"
+# password = "Selvasmani"
+
+# username1 = input("Enter your username: ")
+# password1 = input("Enter you Password: ")
+# if username1 == username and password1 == password:
+#     print("Login Successful.")
+# else:
+#     print("Login failed.")
+
+
